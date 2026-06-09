5185d861f1993ef2e0b41b3f980afc0acd3d0410
Adéla Macečková <adelamaceckova@Adela--MacBook-Air.local>
2026-06-09 16:07:36 +0200

    Expand character archive and add character metadata

---
+++ data/characterMetadata.js
@@ -0,0 +1,25 @@
+export const characterMetadata = {
+  furina: {
+    element: "Hydro",
+    weapon: "Sword",
+    region: "Fontaine",
+  },
+
+  navia: {
+    element: "Geo",
+    weapon: "Claymore",
+    region: "Fontaine",
+  },
+
+  bennett: {
+    element: "Pyro",
+    weapon: "Sword",
+    region: "Mondstadt",
+  },
+
+  neuvillette: {
+    element: "Hydro",
+    weapon: "Catalyst",
+    region: "Fontaine",
+  },
+};
